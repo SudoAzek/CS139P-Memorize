e738ae835c13f9a1bc4aeebf54db1a6c6ea9602d
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2021-06-10 11:12:53 +0900

    Lecture 1: Getting started with SwiftUI

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Azamat Ochilov on 2021/06/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            
+            Text("Hello, Az!")
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Azamat Ochilov on 2021/06/09.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
